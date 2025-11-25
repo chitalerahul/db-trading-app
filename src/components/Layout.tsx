cbbcd96d334e514dc1f5223a2133040ddcdcb44f
chitalerahul <chitalerahul86@gmail.com>
2025-11-25 23:30:17 +0530

    AppBar added  as Header

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,8 +1,17 @@
-import { Button, styled, Typography } from "@mui/material";
-import useThemeContext from "../hooks/useThemeContext";
 import type { ReactElement } from "react";
-import Drawer from "@mui/material/Drawer";
 
+import {
+  AppBar,
+  Box,
+  Button,
+  styled,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import Drawer from "@mui/material/Drawer";
+import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
+
+import useThemeContext from "../hooks/useThemeContext";
 import styles from "./Layout.module.css";
 import Menu from "./Menu";
 
@@ -20,19 +29,40 @@ const StyledDrawer = styled(Drawer)(() => ({
   },
 }));
 
+const StyledAppbar = styled(AppBar)(() => ({
+  width: `calc(100% - ${drawerWidth}px)`,
+}));
+
 const Layout: React.FC<LayoutProps> = ({ children }) => {
-  const { toggleColorMode, mode } = useThemeContext();
+  const { toggleColorMode } = useThemeContext();
 
   return (
-    <div style={{ display: "flex" }}>
+    <div style={{ display: "flex", minHeight: "780px" }}>
+      <StyledAppbar>
+        <Toolbar>
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            <AttachMoneyIcon />
+            <Typography>Trade App</Typography>
+          </Box>
+          <Box sx={{ display: "flex", alignItems: "center", ml: "auto" }}>
+            {" "}
+            {/* ml: 'auto' pushes to the right */}
+            <Button
+              sx={{ align: "right" }}
+              variant="contained"
+              onClick={toggleColorMode}
+            >
+              Toggle Theme
+            </Button>
+          </Box>
+        </Toolbar>
+      </StyledAppbar>
       <StyledDrawer variant="permanent" anchor="left">
         <Menu></Menu>
       </StyledDrawer>
       <div className={styles.pageContent}>
-        <Typography variant="h4">Current Theme: {mode}</Typography>
-        <Button variant="contained" onClick={toggleColorMode}>
-          Toggle Theme
-        </Button>
+        <div className={styles.contentPusher}>&nbsp;</div>
+
         {children}
       </div>
     </div>
