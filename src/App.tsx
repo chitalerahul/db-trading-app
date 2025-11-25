df5382464a58a38f0bd5b275b6e7bec0ca64aebf
chitalerahul <chitalerahul86@gmail.com>
2025-11-25 11:35:40 +0530

    react router dom added with basic routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import About from "./components/About.tsx";
+import Trade from "./components/Trade.tsx";
+import Trades from "./components/Trades.tsx";
+
 function App() {
-  return <h1>Hello react</h1>;
+  return (
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Trades />} />
+          <Route path="/trades" element={<Trades />} />
+          <Route path="/trade/:id" element={<Trade />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Layout>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/About.tsx
@@ -0,0 +1,3 @@
+export default function About() {
+  return <p>This is about section for trade App</p>;
+}
