f0af5ef50297f885b9f1fad2f1fb32ddf317792e
chitalerahul <chitalerahul86@gmail.com>
2025-12-01 10:21:24 +0530

    service worker base path

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,11 @@ async function enableMocking() {
 
   // `worker.start()` returns a Promise that resolves
   // once the Service Worker is up and ready to intercept requests.
-  return worker.start();
+  return worker.start({
+    serviceWorker: {
+      url: "/db-trading-app/mockServiceWorker.js",
+    },
+  });
 }
 
 enableMocking().then(() => {
